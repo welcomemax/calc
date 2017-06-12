ecd84881806ad99ffb0114a551b87860e864bd1e
welcomemax <welcomemax@icloud.com>
2017-06-12 22:06:22 +0300

    readme

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,4 @@
 import { Provider } from 'react-redux'
-//import { Router, browserHistory } from 'react-router'
-//import RootRouter from './router.jsx'
 import configureStore from './store/configure-store.jsx'
 
 import Calc from './components/calc/control.jsx';
--- app/store/configure-store.jsx
+++ app/store/configure-store.jsx
@@ -1,14 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
-//import { syncHistoryWithStore } from 'react-router-redux';
-//import { browserHistory } from 'react-router';
 import rootReducer from './reducers.jsx';
 import thunk from 'redux-thunk';
-//import customMiddleware from './middleware.jsx'
 
 export default function configureStore(initialState) {
     let middleware = [
-        thunk,
-        //customMiddleware
+        thunk
     ];
 
     return createStore(
